4763b1bf3f34caa611068282d7c4d7fc30421089
EduardoJesusGagoRamos <wilberdymr2004@gmail.com>
2022-10-25 06:07:00 -0500

    PROGRAMAS DESARROLLADOS EN CLASE EN NETBEANS Y VISUAL CODE

---
+++ src/SEMANA_03/EJERCICIO_04.java
@@ -0,0 +1,34 @@
+// Crear un array bidimensional de tamaño 7x7 y rellénalo de forma que los elementos de la diagonal principal sean 1 y el resto 0. Muestra el array por pantalla en forma cuadrado
+
+package SEMANA_03;
+import java.util.Arrays;
+
+/**
+ *
+ * @author GAGO RAMOS EDUARDO JESÚS
+ */
+public class EJERCICIO_04 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int [][] matriz = new int[7][7];
+        int f;
+        //Relleno la diagonal principal con 1
+        //Solo tengo que recorrer las casillas en las que el numero de filas es
+        //igual que el numero de columna : matriz[0][0]=matriz[1][1]...=matriz[6][6]=1
+        for(f=0;f <matriz.length;f++){
+            matriz[f][f] = 1;
+        }
+        
+        //muestro las filas de la matriz haciendo uso de la libreria "Arrays"
+        for(f=0; f < matriz.length;f++){
+            System.out.println(Arrays.toString(matriz[f]));
+        }       
+    }
+    
+}
+    
+
